4d5bce641e12abf6be92494079432d7b700ca6ce
lu lin <wenhelinlu@outlook.com>
2018-06-14 16:28:44 +0800

    1. 优化正文界面全屏显示效果； 2. 正文底部工具栏和系统状态栏在点击正文时一同显示或隐藏。

--- app/src/main/java/com/lm/ll/spark/DisplayArticleActivity.kt
+++ app/src/main/java/com/lm/ll/spark/DisplayArticleActivity.kt
@@ -3,6 +3,7 @@ package com.lm.ll.spark
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.os.Handler
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.view.View
@@ -37,31 +38,22 @@ class DisplayArticleActivity : AppCompatActivity() {
     private lateinit var article: Article
     //此文章下的首层评论
     private var comments: ArrayList<Article> = ArrayList()
-    //评论adapter
+    //评论列表adapter
     private lateinit var commentsAdapter: CommentRecyclerViewAdapter
 
-    private var isHidding = false
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        setContentView(R.layout.activity_display_article)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
-
-        Realm.init(this)
-
-        initData()
-
-        initView()
-
-        loadText()
-    }
-
-    private fun hide() {
-        // Hide UI first
-        supportActionBar?.hide()
+    /**
+     * @desc 用于延迟触发隐藏状态栏、导航栏等操作
+     * @author ll
+     * @time 2018-06-14 16:28
+     */
+    private val mHideHandler = Handler()
+    /**
+     * @desc 延迟隐藏系统状态栏和导航栏
+     * @author ll
+     * @time 2018-06-14 15:53
+     */
+    private val mHidePart2Runnable = Runnable {
+        // Delayed removal of status and navigation bar
 
         // Note that some of these constants are new as of API 16 (Jelly Bean)
         // and API 19 (KitKat). It is safe to use them, as they are inlined
@@ -73,26 +65,151 @@ class DisplayArticleActivity : AppCompatActivity() {
                 View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
                 View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
                 View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-        isHidding = true
     }
 
+    /**
+     * @desc 延迟显示toolbar和底部控件
+     * @author ll
+     * @time 2018-06-14 15:50
+     */
+    private val mShowPart2Runnable = Runnable {
+        // Delayed display of UI elements
+        supportActionBar?.show()
+        fullscreen_content_controls.visibility = View.VISIBLE
+    }
+
+    /**
+     * @desc 控制全屏显示
+     * @author ll
+     * @time 2018-06-14 15:51
+     */
+    private var mVisible: Boolean = false
+    /**
+     * @desc 隐藏状态栏和导航栏线程
+     * @author ll
+     * @time 2018-06-14 15:52
+     */
+    private val mHideRunnable = Runnable { hide() }
+    /**
+     * Touch listener to use for in-layout UI controls to delay hiding the
+     * system UI. This is to prevent the jarring behavior of controls going away
+     * while interacting with activity UI.
+     */
+    private val mDelayHideTouchListener = View.OnTouchListener { _, _ ->
+        if (DisplayArticleActivity.AUTO_HIDE) {
+            delayedHide(DisplayArticleActivity.AUTO_HIDE_DELAY_MILLIS)
+        }
+        false
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_display_article)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        mVisible = true
+
+        // Set up the user interaction to manually show or hide the system UI.
+        tvText.setOnClickListener { toggle() }
+
+        // Upon interacting with UI controls, delay any scheduled hide()
+        // operations to prevent the jarring behavior of controls going away
+        // while interacting with the UI.
+        toolbar_bottom_text.setOnTouchListener(mDelayHideTouchListener)
+
+        Realm.init(this)
+
+        initData()
+
+        initView()
+
+        loadText()
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+
+        // Trigger the initial hide() shortly after the activity has been
+        // created, to briefly hint to the user that UI controls
+        // are available.
+        delayedHide(100)
+    }
+
+    /**
+     * @desc 控制全屏显示
+     * @author ll
+     * @time 2018-06-14 15:38
+     */
+    private fun toggle() {
+        if (mVisible) {
+            hide()
+        } else {
+            show()
+        }
+    }
+
+    /**
+     * @desc 隐藏状态栏和导航栏，全屏显示
+     * @author ll
+     * @time 2018-06-14 15:38
+     */
+    private fun hide() {
+        // Hide UI first
+        supportActionBar?.hide()
+        fullscreen_content_controls.visibility = View.GONE
+        mVisible = false
+
+        // Schedule a runnable to remove the status and navigation bar after a delay
+        mHideHandler.removeCallbacks(mShowPart2Runnable)
+        mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY.toLong())
+    }
+
+    /**
+     * @desc 显示状态栏和导航栏
+     * @author ll
+     * @time 2018-06-14 15:47
+     */
     private fun show() {
         // Show the system bar
         fullscreen_content.systemUiVisibility =
                 View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
                 View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        mVisible = true
 
-        isHidding = false
+        // Schedule a runnable to display UI elements after a delay
+        mHideHandler.removeCallbacks(mHidePart2Runnable)
+        mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY.toLong())
     }
 
-    private fun toggle() {
+    /**
+     * Schedules a call to hide() in [delayMillis], canceling any
+     * previously scheduled calls.
+     */
+    private fun delayedHide(delayMillis: Int) {
+        mHideHandler.removeCallbacks(mHideRunnable)
+        mHideHandler.postDelayed(mHideRunnable, delayMillis.toLong())
+    }
 
-        if (isHidding) {
-            show()
-        } else {
-            hide()
+    companion object {
+        /**
+         * Whether or not the system UI should be auto-hidden after
+         * [AUTO_HIDE_DELAY_MILLIS] milliseconds.
+         */
+        private const val AUTO_HIDE = true
 
-        }
+        /**
+         * If [AUTO_HIDE] is set, the number of milliseconds to wait after
+         * user interaction before hiding the system UI.
+         */
+        private const val AUTO_HIDE_DELAY_MILLIS = 3000
+
+        /**
+         * Some older devices needs a small delay between UI widget updates
+         * and a change of the status and navigation bar.
+         */
+        private const val UI_ANIMATION_DELAY = 300
     }
 
     /**
@@ -122,6 +239,7 @@ class DisplayArticleActivity : AppCompatActivity() {
             it.title = article.title
         }
 
+        //点击正文显示或隐藏状态栏和导航栏
         tvText.setOnClickListener {
             toggle()
         }
@@ -169,19 +287,6 @@ class DisplayArticleActivity : AppCompatActivity() {
         this.recyclerViewComment.addItemDecoration(DashlineItemDecoration())
         this.recyclerViewComment.layoutManager = linearLayoutManager
         this.recyclerViewComment.isNestedScrollingEnabled = false
-
-        //显示或隐藏底栏
-        scrollviewText.setOnScrollChangeListener { _, _, scrollY, _, oldScrollY ->
-            var isShow = true
-            if (scrollY - oldScrollY > 0) {
-                isShow = false
-
-            } else if (oldScrollY - scrollY > 0) {
-                isShow = true
-
-            }
-            showBottomToolbar(isShow)
-        }
     }
 
     /**
@@ -192,7 +297,7 @@ class DisplayArticleActivity : AppCompatActivity() {
     private fun loadText() {
         val deferredLoad = async(CommonPool) {
             val spider = Spider()
-            if (isClassic) {
+            if (isClassic) { //经典文库的文章解析方式不同
                 article = spider.scratchClassicEroticaArticleText(article)
             } else {
                 article = spider.scratchText(article, comments) //正文中可能也包含链接（比如精华区）
@@ -235,25 +340,5 @@ class DisplayArticleActivity : AppCompatActivity() {
             recyclerViewComment.adapter = commentsAdapter
             recyclerViewComment.adapter.notifyDataSetChanged()
         }
-    }
-
-
-    /**
-     * @desc 滑动正文内容时显示或隐藏底栏
-     * @author ll
-     * @time 2018-06-03 08:14
-     */
-    private fun showBottomToolbar(isShow: Boolean) {
-//        if (isShow) {
-//            toolbar_bottom_text.visibility = View.VISIBLE
-//            val animation = AnimationUtils.loadAnimation(this@ArticleDisplayActivity, R.anim.fab_jump_from_down)
-//            toolbarBottomText!!.startAnimation(animation)
-//        } else {
-//            toolbar_bottom_text.visibility = View.GONE
-//            val animation = AnimationUtils.loadAnimation(this@ArticleDisplayActivity, R.anim.fab_jump_to_down)
-//            toolbarBottomText!!.startAnimation(animation)
-//        }
-
-        toolbar_bottom_text.visibility = if (isShow) View.VISIBLE else View.GONE
     }
 }
