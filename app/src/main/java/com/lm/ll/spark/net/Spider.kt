ec6873503fed87aae277c73c1ef5282fb31d7565
LinLu <wenhelinlu@gmail.com>
2019-01-14 20:46:24 +0800

    优化代码

--- app/src/main/java/com/lm/ll/spark/net/Spider.kt
+++ app/src/main/java/com/lm/ll/spark/net/Spider.kt
@@ -63,7 +63,7 @@ class Spider {
                 val nodes = doc.select("div#d_list")[0].childNodes()[1].childNodes()
 
                 for (node in nodes) {
-                    if(node.childNodes() != null && node.childNodeSize() > 5){
+                    if (node.childNodes() != null && node.childNodeSize() > 5) {
                         val links = node.childNodes()[5] as Element
                         parseOnlineVideoList(links, mList)
                     }
@@ -446,7 +446,7 @@ class Spider {
              * 因为不同的文章可能段落符号不一致，两个\r\n之间可能有0到多个空格，影响下一步的替换处理。所以先将\r\n和\r\n之间的空格去掉再匹配，统一将段落转换成\r\n\r\n形式
              */
             //文本中的6park.com或者cool18.com替换为段落标记，起到分段效果
-            val puredText = originalText.replace("6park.com", paragraphFlag, true).replace("6parker.com", paragraphFlag,true).replace("cool18.com",paragraphFlag,true)
+            val puredText = originalText.replace("6park.com", paragraphFlag, true).replace("6parker.com", paragraphFlag, true).replace("cool18.com", paragraphFlag, true)
             //先去除空行标记（某些文章（如【只贴精品-马艳丽1-4）会因为空行标记导致误判断为含段落标记，从而清除换行标记后，排版混乱）
             val removedEmptyLineText = Regex(emptyLineFlagPattern).replace(puredText, "")
             //判断文本中段落标记（\r\n\r\n）个数，大于某个值，则处理，否则不处理
@@ -469,13 +469,13 @@ class Spider {
          */
         fun scratchClassicEroticaArticleText(doc: Document, article: Article): Article {
             try {
-                val elements = doc.getElementsByTag("p")
                 val stringBuilder = StringBuilder()
-                for (e in elements) {
-                    if (e.childNodeSize() == 2 && e.childNodes()[0] is TextNode) {
-                        stringBuilder.appendln((e.childNodes()[0] as TextNode).text())
-                    }
+                doc.getElementsByTag("p").filter { e -> e.childNodeSize() == 2 && e.childNode(0) is TextNode }.forEach { e ->
+                    stringBuilder.appendln((e.childNodes()[0] as TextNode).text())
                 }
+//                for (e in elements) {
+//                    stringBuilder.appendln((e.childNodes()[0] as TextNode).text())
+//                }
                 article.text = stringBuilder.toString().convertToSimplifiedChinese()
                 return article
             } catch (t: Throwable) {
